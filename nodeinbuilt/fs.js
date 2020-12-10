79fd8da91345de47ddba7b6f1e80d9179996ce40
aakashdeveloper <ahanda205@gmail.com>
2020-12-10 19:31:45 +0530

    a

--- nodeinbuilt/fs.js
+++ nodeinbuilt/fs.js
@@ -14,7 +14,19 @@ fs.appendFile('mytext.txt','This is from NodeJs \n',function(err){
 })*/
 
 //readfile
-fs.readFile('mytext.txt','utf-8',function(err,data){
+/*fs.readFile('mytext.txt','utf-8',function(err,data){
     if(err) throw err;
     console.log(data)
+})
+*/
+//rename
+/*
+fs.rename('mytext.txt','myfile.txt',function(err){
+    if(err) throw err;
+    console.log("File Renamed")
+})*/
+
+fs.unlink('mytext.txt',function(err){
+    if(err) throw err;
+    console.log("File Deleted")
 })
